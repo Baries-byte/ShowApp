95b5d54b4d2d42a33fc81155117998c2dac87d84
Baries-byte <bagusaries26@gmail.com>
2023-02-15 13:41:02 +0700

    init

---
+++ src/components/PokemonCards.vue
@@ -0,0 +1,18 @@
+<script setup>
+// import {ref, watch} from "vue"
+import axios from "axios"
+
+// const pokemons = ref(null)
+// const page = ref(0)
+
+const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=10")
+
+console.log(response)
+</script>
+
+<template>
+	<div>
+        <h1>Pokemon Cards</h1>
+        {{ response.data.results.length }}
+    </div>
+</template>
